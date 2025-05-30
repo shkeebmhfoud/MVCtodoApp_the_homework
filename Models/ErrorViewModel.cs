a2da4a854e2ee5aeb6836f278007e48e5d655d6a
unknown <shkeebmhfoze01@gmail.com>
2025-05-30 21:23:38 +0300

    init commit (contains add, and complete task functions)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVCtodoApp_the_homework.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
